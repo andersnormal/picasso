d08effb73fd96981363cee827ab5c54c16aca1d7
Sebastian Döll <sebastian@katallaxie.me>
2022-07-15 05:50:33 +0000

    (wip) run plugins

--- pkg/runner/funcs.go
+++ pkg/runner/funcs.go
@@ -3,12 +3,15 @@ package runner
 import (
 	"context"
 	"io"
+	"log"
 	"math"
 	"strings"
 	"time"
 
+	"github.com/andersnormal/picasso/pkg/plugin"
 	"github.com/andersnormal/picasso/pkg/spec"
 	"github.com/andersnormal/picasso/pkg/templr"
+
 	"mvdan.cc/sh/expand"
 	"mvdan.cc/sh/interp"
 	"mvdan.cc/sh/syntax"
@@ -62,6 +65,15 @@ func RunTask() RunFunc {
 				timeout = time.Duration(s.TimeoutInSeconds) * time.Second
 			}
 
+			if s.Uses != "" {
+				err := runPlugin(c.Context(), s.Uses, c.WorkingDir(), c.runner.Stdin(), c.runner.Stdout(), c.runner.Stderr(), c.Env())
+				if err != nil {
+					return err
+				}
+
+				continue
+			}
+
 			if err := runCmd(c.Context(), timeout, c.WorkingDir(), c.runner.Stdin(), c.runner.Stdout(), c.runner.Stderr(), c.Env(), cmds...); err != nil {
 				if s.ContinueOnError {
 					continue
@@ -73,6 +85,29 @@ func RunTask() RunFunc {
 
 		return c.Next()
 	}
+}
+
+func runPlugin(ctx context.Context, name string, dir WorkingDir, stdin io.Reader, stdout io.Writer, stderr io.Writer, env []string) error {
+	m := &plugin.Meta{Path: name}
+	f := m.Factory()
+
+	p, err := f()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer p.Close()
+
+	_, err = p.Execute(plugin.ExecuteRequest{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = p.Stop()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
 }
 
 func runCmd(ctx context.Context, timeout time.Duration, dir WorkingDir, stdin io.Reader, stdout io.Writer, stderr io.Writer, env []string, cmds ...string) error {
