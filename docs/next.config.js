8061cfebbae89d2028b3dd773602e23a52d7a76b
Sebastian Döll <sebastian@katallaxie.me>
2022-07-18 09:52:19 +0000

    (wip) fix base path

--- docs/next.config.js
+++ docs/next.config.js
@@ -6,7 +6,7 @@ const withNextra = require('nextra')({
     unstable_staticImage: true,
 })
 module.exports = withNextra({ 
-    basePath: isProd ? "/picasso/" : "",
+    basePath: isProd ? "/picasso" : "",
     assetPrefix: isProd ? "/picasso/" : "", experimental: {
     images: {
         unoptimized: true
